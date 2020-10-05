44eb246462c2d1af5cdca4eff18f139966d409b0
amlagsac <amlagsac@student.apc.edu.ph>
2020-10-05 11:00:44 +0800

    Added Edit and Update function in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        $categories = ProductCategory::all();
+
+        return view('inventory.products.edit', compact('product', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+        $product->update($request->all());
+
+        return redirect()
+            ->route('products.index')
+            ->withStatus('Product updated successfully.');
+    }
 }
