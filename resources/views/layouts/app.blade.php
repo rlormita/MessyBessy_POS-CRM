a45e9c7e2cb6cce20caff9b41ca00904debc285c
cmdinglasan <cmdinglasan@student.apc.edu.ph>
2020-10-06 14:01:53 +0800

    Updated App Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,10 +53,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ $user = Auth::user() }}
                                     {{ Auth::user()->firstName }}
-                                    {{ Auth::user()->firstame }}
-                                    {{ $user = Auth::user()-> firstName }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
