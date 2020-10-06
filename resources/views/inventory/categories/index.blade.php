58b729e50c2f28113f916d6851bcf3de2d51b448
cmdinglasan <cmdinglasan@student.apc.edu.ph>
2020-10-06 23:48:07 +0800

    Fixed error in display for Categories

--- resources/views/inventory/categories/index.blade.php
+++ resources/views/inventory/categories/index.blade.php
@@ -14,68 +14,6 @@
                 </div>
                 <div class="card-body">
                     @include('alerts.success')
-                    <div class="col-4 text-right">
-                        <a href="{{ route('categories.create') }}" class="btn btn-sm btn-primary">New Category</a>
-                    </div>
-                    <div class="card-body">
-                        @include('alerts.success')
-
-                        <div class="">
-                            <table class="table tablesorter " id="">
-                                <thead class=" text-primary">
-                                    <th scope="col">Name</th>
-                                    <th scope="col">products</th>
-                                    <th scope="col">Total Stock</th>
-                                    <th scope="col">Defective Stock</th>
-                                    <th scope="col">Average Price of Product</th>
-                                    <th scope="col"></th>
-                                </thead>
-                                <tbody>
-                                    @foreach ($categories as $category)
-                    <div class="">
-                        <table class="table tablesorter " id="">
-                            <thead class=" text-primary">
-                                <th scope="col">Name</th>
-                                <th scope="col">products</th>
-                                <th scope="col">Total Stock</th>
-                                <th scope="col">Minimum Stocks</th>
-                                <th scope="col">Price of Product</th>
-                                <th scope="col"></th>
-                            </thead>
-                            <tbody>
-                                @foreach ($categories as $category)
-                                    <tr>
-                                        <td>{{ $category->name }}</td>
-                                        <td>{{ count($category->products) }}</td>
-                                        <td>{{ $category->products->sum('stock') }}</td>
-                                        <td>{{ $category->products->sum('stock_defective') }}</td>
-                                        <td>{{ $category->products->avg('price') }}</td>
-                                        <td>{{($category->products->avg('price')) }}</td>
-                                        <td class="td-actions text-right">
-                                            <a href="{{ route('categories.show', $category) }}" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="More Details">
-                                                <i class="tim-icons icon-zoom-split"></i>
-                                            </a>
-                                            <a href="{{ route('categories.edit', $category) }}" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Edit Category">
-                                                <i class="tim-icons icon-pencil"></i>
-                                            </a>
-                                            <form action="{{ route('categories.destroy', $category) }}" method="post" class="d-inline">
-                                                @csrf
-                                                @method('delete')
-                                                <button type="button" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Delete Category" onclick="confirm('Are you sure you want to delete this category? All products belonging to it will be deleted and the records that contain it will not be accurate.') ? this.parentElement.submit() : ''">
-                                                    <i class="tim-icons icon-simple-remove"></i>
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    @include('alerts.success')
 
                     <div class="">
                         <table class="table tablesorter " id="">
