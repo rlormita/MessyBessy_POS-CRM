550280272e45d9f2013b7086647b834d3761d3cd
Jesus Bermudo <jmbermudo@student.apc.edu.ph>
2020-10-08 17:11:52 +0800

    navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,9 +18,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&display=swap" rel="stylesheet">
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div id="app">
         <!-- nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -72,14 +75,14 @@
             </div>
         </nav -->
 
-<<<<<<< HEAD
- 
         <main class="py-4">
-=======
-        <main class="py-4 main-content">
->>>>>>> 6e01b9e431f1ea31beb2da29f27a2355dd470572
-            @yield('content')
+
+            <main class="py-4 main-content">
+
+                @yield('content')
+            </main>
         </main>
     </div>
 </body>
+
 </html>
