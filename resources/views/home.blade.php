2101ba424634d798dc86d642d9cfd3c3ee6ec0e3
cmdinglasan <cmdinglasan@student.apc.edu.ph>
2020-10-08 22:33:35 +0800

    Minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,18 +22,21 @@
                     @else
                         <img src="{{ url('img/uploads/profile_image/') }}/{{ Auth::user()->profile_image }}" />
                     @endif
-                </div>
 
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
-                                     document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('profile') }}">
+                            {{ __('Profile') }}
+                        </a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
