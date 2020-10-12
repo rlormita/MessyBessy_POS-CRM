c355ad56bfd66410155764681c24e50886b7629e
cmdinglasan <cmdinglasan@student.apc.edu.ph>
2020-10-12 13:47:31 +0800

    Fix 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,8 +34,6 @@
                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                      document.getElementById('logout-form').submit();">
                         {{ __('Logout') }}
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
                     </a>
                 </div>
 
