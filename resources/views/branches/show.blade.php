e3b6ec449c273afc076efcd463dc3268f3542ff3
ebrocamora <ebrocamora@student.apc.edu.ph>
2020-10-15 22:52:24 +0800

    Removed the redundant data in branches that is presented in index

---
+++ resources/views/branches/show.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.app', ['page' => 'Branch Information', 'pageSlug' => 'branches'])
+
+@section('content')
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{url('/dashboard')}}">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <img style="width: 90%;" src="{{ asset('img/icon.png') }}" />
+            </div>
+            <div class="sidebar-brand-text mx-3">EasyBessy Admin</div>
+        </a>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+            <a class="nav-link" href="{{url('/dashboard')}}">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Dashboard</span></a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Interface
+        </div>
+
+
+        <div class="nav-items">
+            <!-- Nav Item - Product Menu -->
+            <li style="margin-bottom: 0px !important;" class="nav-item">
+                <a style="padding: 15px; padding-bottom:0px" class="nav-link" href="{{url('/products')}}">
+                    <i class="fas fa-shopping-bag"></i>
+                    <span>Products</span>
+                </a>
+            </li>
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li style="margin-bottom: 0px !important;" class="nav-item">
+                <a style="padding: 15px; padding-bottom:0px" class="nav-link" href="{{url('/categories')}}">
+                    <i class="fas fa-table"></i>
+                    <span>Categories</span>
+                </a>
+            </li>
+            <li style="margin-bottom: 0px !important;" class="nav-item">
+                <a style="padding: 15px; padding-bottom:0px" class="nav-link" href="{{url('/stocks')}}">
+                    <i class="fas fa-table"></i>
+                    <span>Stocks</span>
+                </a>
+            </li>
+            <li style="margin-bottom: 0px !important;" class="nav-item">
+                <a style="padding: 15px; padding-bottom:0px" class="nav-link" href="{{url('/branches')}}">
+                    <i class="fas fa-table"></i>
+                    <span>Branches</span>
+                </a>
+            </li>
+        </div>
+
+
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Addons
+        </div>
+
+
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+
+    </ul>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Category Information</h4>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <th>Branch ID</th>
+                            <th>Branch Name</th>
+                            <th>Street</th>
+                            <th>City</th>
+                            <th>State</th>
+                            <th>Post Code</th>
+                            <th>Country</th>
+                            <th>Contact Number</th>
+                            <th>Operating Hours</th>
+                            <th>Additional Information</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                    <td>{{ $branch->id }}</td>
+                                    <td>{{ $branch->branch_name }}</td>
+                                    <td>{{ $branch->branch_street }}</td>
+                                    <td>{{ $branch->branch_city }}</td>
+                                    <td>{{ $branch->branch_state }}</td>
+                                    <td>{{ $branch->branch_post_code }}</td>
+                                    <td>{{ $branch->branch_country }}</td>
+                                    <td>{{ $branch->branch_contact_number }}</td>
+                                    <td>{{ $branch->branch_operating_hours }}</td>
+                                    <td>{{ $branch->branch_other_info }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
